7a70ec507cf75318447be713cfef934f373c7cf2
Andre140888 <102464@glr.nl>
2025-09-08 12:38:43 +0000

    main js made

---
+++ animate.js
@@ -0,0 +1,11 @@
+let dog = document.getElementById("dog");
+let boss = document.getElementById("boss");
+let bird = document.getElementById("bird");
+let shield = document.getElementById("shield");
+let slime = document.getElementById("slime");
+let sword = document.getElementById("sword");
+
+
+iages = [
+    'dog'
+]
